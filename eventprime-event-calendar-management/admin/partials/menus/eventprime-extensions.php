873076bf033eb2d5febd4dead6bb89a5fea87b7e
Sean Murphy <sean@iamseanmurphy.com>
2025-04-04 12:19:16 +0000

    4.0.9.2

--- eventprime-event-calendar-management/admin/partials/menus/eventprime-extensions.php
+++ eventprime-event-calendar-management/admin/partials/menus/eventprime-extensions.php
@@ -3,11 +3,11 @@ $global_function =  new Eventprime_Basic_Functions();
 ?>
 
 <div class="emagic">
-<!--    <div class="ep-exts-bundle-banner ep-box-wrap ep-text-center ep-mt-5">
+    <div class="ep-exts-bundle-banner ep-box-wrap ep-text-center ep-mt-5">
         <a href="https://theeventprime.com/all-extensions/" target="_blank" class="ep-inline-block">
-            <img class="ep-extension-bundle" alt="<?php echo esc_attr( 'EventPrime Extension Bundle');?>" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'images/ep-extension-banner.png'); ?>" >
+            <img class="ep-extension-bundle" alt="<?php echo esc_attr( 'EventPrime Extension Bundle');?>" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'images/ep-extensions-banner.png'); ?>" >
         </a>
-    </div>-->
+    </div>
 
     <div class="ep-extensions-filters ep-box-wrap ep-mt-5">
         <div class="ep-box-row ep-mb-3">    
@@ -90,11 +90,11 @@ $global_function =  new Eventprime_Basic_Functions();
         </div>
     </div>
     
-<!--    <div class="ep-exts-bundle-banner ep-box-wrap ep-text-center ep-mt-5">
+    <div class="ep-exts-bundle-banner ep-box-wrap ep-text-center ep-mt-5">
         <a href="https://theeventprime.com/all-extensions/" target="_blank" class="ep-inline-block">
-            <img  class="ep-extension-bundle" alt="<?php echo esc_attr( 'EventPrime Extension Bundle');?>" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'images/ep-extension-banner.png'); ?>" >
+            <img  class="ep-extension-bundle" alt="<?php echo esc_attr( 'EventPrime Extension Bundle');?>" src="<?php echo esc_url( plugin_dir_url( __DIR__ ) . 'images/ep-extensions-banner.png'); ?>" >
         </a>
-    </div>-->
+    </div>
     
 </div>
 
